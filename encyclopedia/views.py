c6293bcb585ce746aea732f74f99fc9e6b301fb2
Matthew Rawlings <matthew.rawlings6@gmail.com>
2021-08-11 17:52:55 +0200

    Working on search form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
 
 from . import util
 
+class NewQueryForm(forms.Form):
+    query = forms.CharField(label="query", widget=forms.TextInput({"placeholder": "Search Encyclopedia", "class": "search"}))
 
 def index(request):
+    if request.method:
+        form: NewQueryForm(request.GET)
+
+        if form.is_valid():
+            return HttpResponseRedirect("display/{NewQueryForm.value}")
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewQueryForm()
     })
 
 def display(request, title):
